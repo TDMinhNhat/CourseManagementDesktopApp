f3274bf0b7036043f08718b4011421f681137e36
Dell <Dell@SKYHEROBRINE>
2024-06-13 17:02:23 +0700

    Add project files.

---
+++ CourseManagementDesktopApp/FLogin.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CourseManagementDesktopApp
+{
+    partial class FLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogin));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txbAccountName = new System.Windows.Forms.TextBox();
+            this.pcbAccountName = new System.Windows.Forms.PictureBox();
+            this.lblAccountName = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.pcbPassword = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAccountName)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPassword)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txbAccountName);
+            this.panel1.Controls.Add(this.pcbAccountName);
+            this.panel1.Controls.Add(this.lblAccountName);
+            this.panel1.Location = new System.Drawing.Point(159, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(398, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // txbAccountName
+            // 
+            this.txbAccountName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbAccountName.Location = new System.Drawing.Point(135, 14);
+            this.txbAccountName.Name = "txbAccountName";
+            this.txbAccountName.Size = new System.Drawing.Size(260, 20);
+            this.txbAccountName.TabIndex = 2;
+            // 
+            // pcbAccountName
+            // 
+            this.pcbAccountName.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pcbAccountName.BackgroundImage")));
+            this.pcbAccountName.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pcbAccountName.Location = new System.Drawing.Point(3, 3);
+            this.pcbAccountName.Name = "pcbAccountName";
+            this.pcbAccountName.Size = new System.Drawing.Size(41, 41);
+            this.pcbAccountName.TabIndex = 1;
+            this.pcbAccountName.TabStop = false;
+            // 
+            // lblAccountName
+            // 
+            this.lblAccountName.AutoSize = true;
+            this.lblAccountName.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblAccountName.Location = new System.Drawing.Point(53, 17);
+            this.lblAccountName.Name = "lblAccountName";
+            this.lblAccountName.Size = new System.Drawing.Size(76, 13);
+            this.lblAccountName.TabIndex = 0;
+            this.lblAccountName.Text = "Tên tài khoản:";
+            this.lblAccountName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbPassword);
+            this.panel2.Controls.Add(this.pcbPassword);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(159, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(398, 47);
+            this.panel2.TabIndex = 1;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbPassword.Location = new System.Drawing.Point(135, 14);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(260, 20);
+            this.txbPassword.TabIndex = 2;
+            this.txbPassword.UseSystemPasswordChar = true;
+            // 
+            // pcbPassword
+            // 
+            this.pcbPassword.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pcbPassword.BackgroundImage")));
+            this.pcbPassword.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pcbPassword.Location = new System.Drawing.Point(3, 3);
+            this.pcbPassword.Name = "pcbPassword";
+            this.pcbPassword.Size = new System.Drawing.Size(41, 41);
+            this.pcbPassword.TabIndex = 1;
+            this.pcbPassword.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(53, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mật khẩu:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 141);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Lime;
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Red;
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogin.Location = new System.Drawing.Point(78, 192);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 64);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(374, 192);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(120, 64);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // FLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 263);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập Tài Khoản";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbAccountName)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPassword)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblAccountName;
+        private System.Windows.Forms.PictureBox pcbAccountName;
+        private System.Windows.Forms.TextBox txbAccountName;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.PictureBox pcbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ CourseManagementDesktopApp/FLogin.cs
@@ -0,0 +1,30 @@
+﻿using CourseManagementDesktopApp.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseManagementDesktopApp
+{
+    public partial class FLogin : Form
+    {
+        public FLogin()
+        {
+            InitializeComponent();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FLogin));
+
+            Bitmap bitmapLogin = new Bitmap(((System.Drawing.Image)(resources.GetObject("btnLogin.Image"))), new Size(45, 45));
+            this.btnLogin.Image = (Image)bitmapLogin;
+            this.btnLogin.ImageAlign = ContentAlignment.MiddleLeft;            
+            
+            Bitmap bitmap = new Bitmap(((System.Drawing.Image)(resources.GetObject("btnExit.Image"))), new Size(45, 45));
+            this.btnExit.Image = (Image) bitmap;
+            this.btnExit.ImageAlign = ContentAlignment.MiddleLeft;
+        }
+    }
+}
